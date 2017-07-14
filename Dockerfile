00ac72e2ed12e48db8036182acdbd619c6ecfcff
David Hoepelman <david.hoepelman@ordina.nl>
2017-07-14 17:25:21 +0200

    Add git

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 #
 # Scala and sbt Dockerfile
 #
-# https://github.com/spikerlabs/scala-sbt (based on https://github.com/hseeberger/scala-sbt)
+# https://github.com/code-star/circleci-scala-sbt-git (based on https://github.com/spikerlabs/scala-sbt)
 #
 
 # Pull base image
@@ -9,12 +9,14 @@ FROM  openjdk:8-jre-alpine
 
 ARG SCALA_VERSION
 ARG SBT_VERSION
+ARG GIT_VERSION
 
 ENV SCALA_VERSION ${SCALA_VERSION:-2.12.2}
 ENV SBT_VERSION ${SBT_VERSION:-0.13.15}
+ENV GIT_VERSION ${GIT_VERSION:-2.13.2}
 
 RUN \
-  echo "$SCALA_VERSION $SBT_VERSION" && \
+  echo "scala $SCALA_VERSION sbt $SBT_VERSION git $GIT_VERSION" && \
   mkdir -p /usr/lib/jvm/java-1.8-openjdk/jre && \
   touch /usr/lib/jvm/java-1.8-openjdk/jre/release && \
   apk add --no-cache bash && \
@@ -29,3 +31,6 @@ RUN \
   $(mv /usr/local/sbt-launcher-packaging-$SBT_VERSION /usr/local/sbt || true) \
   ln -s /usr/local/sbt/bin/* /usr/local/bin/ && \
   sbt sbt-version
+
+RUN \
+  apk add --no-cache git=${GIT_VERSION}-r0
--- build_multiple.sh
+++ build_multiple.sh
@@ -4,15 +4,25 @@ scala_versions=(
   2.12.2
 )
 sbt_versions=(
-  0.13.12
-  0.13.13
   0.13.15
 )
+# Pick from https://pkgs.alpinelinux.org/packages?name=git
+git_versions=(
+  2.13.2
+)
+
 for scala_version in ${scala_versions[@]}; do
   for sbt_version in ${sbt_versions[@]}; do
-    version=scala-${scala_version}-sbt-${sbt_version}
-    docker build -t spikerlabs/scala-sbt:${version} --build-arg SCALA_VERSION=${scala_version} --build-arg SBT_VERSION=${sbt_version} .
-    docker push spikerlabs/scala-sbt:${version}
-    echo "Built ${version}"
+    for git_version in ${git_versions[@]}; do
+      version=scala-${scala_version}-sbt-${sbt_version}-git-$git_version
+      docker build \
+        -t codestar/circleci-scala-sbt-git:${version} \
+        --build-arg SCALA_VERSION=${scala_version} \
+        --build-arg SBT_VERSION=${sbt_version} \
+        --build-arg GIT_VERSION=${git_version} \
+        .
+      docker push codestar/circleci-scala-sbt-git:${version}
+      echo "Built ${version}"
+    done
   done
 done
